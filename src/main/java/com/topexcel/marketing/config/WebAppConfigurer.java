b2063820316adbd586fd4f0ea1ca433385d53953
Casongorgor <116815480@qq.com>
2017-05-25 14:12:40 +0800

    提交基础框架

---
+++ src/main/java/com/topexcel/marketing/config/WebAppConfigurer.java
@@ -0,0 +1,23 @@
+package com.topexcel.marketing.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+
+@Configuration
+public class WebAppConfigurer extends WebMvcConfigurerAdapter {
+
+    /**
+     * 实现跨域配置
+     */
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")//这里填写你允许进行跨域的主机ip
+                .allowCredentials(true)
+                .allowedMethods("GET", "POST", "DELETE", "PUT")//允许的访问方法
+                .maxAge(3600);//Access-Control-Max-Age 用于 CORS 相关配置的缓存
+    }
+
+}
